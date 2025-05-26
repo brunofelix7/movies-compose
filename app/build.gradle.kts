c92faa263a5a13442d6f90591e6a3516e9ace202
bruno <bruno.felix@fuze.cc>
2025-05-26 11:45:09 -0300

    :pushpin: Upgrading compileSdk to 36 (Android 16)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,14 +16,14 @@ properties.load(FileInputStream(apiKeyFile))
 
 android {
     namespace = "dev.brunofelix.pmovie"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "dev.brunofelix.pmovie"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
         testInstrumentationRunner = "dev.brunofelix.pmovie.test_util.HiltTestRunner"
 
         vectorDrawables {
