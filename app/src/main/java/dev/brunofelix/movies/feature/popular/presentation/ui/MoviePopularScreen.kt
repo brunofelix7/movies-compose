ea4cecf55fe044eaf93a4762b410bc00e50b60b0
Bruno Felix <brunofelix.dev@gmail.com>
2026-06-20 12:11:11 -0300

    💄 Updating scrolling on Populars and Upcoming screens and adding GradientBackground to them

--- app/src/main/java/dev/brunofelix/movies/feature/popular/presentation/ui/MoviePopularScreen.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/popular/presentation/ui/MoviePopularScreen.kt
@@ -1,35 +1,48 @@
 package dev.brunofelix.movies.feature.popular.presentation.ui
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.paging.compose.collectAsLazyPagingItems
 import dev.brunofelix.movies.R
+import dev.brunofelix.movies.core.presentation.components.GradientBackground
 import dev.brunofelix.movies.core.presentation.components.MainContent
 import dev.brunofelix.movies.core.presentation.components.MainTopBar
 import dev.brunofelix.movies.feature.popular.presentation.state.MoviePopularUiState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MoviePopularScreen(
     uiState: MoviePopularUiState,
     onItemClick: (id: Long) -> Unit
 ) {
     val movies = uiState.movies.collectAsLazyPagingItems()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
-    Scaffold(
-        topBar = {
-            MainTopBar(
-                title = stringResource(R.string.populars)
-            )
-        },
-        content = { innerPadding ->
-            MainContent(
-                paging = movies,
-                paddingValues = innerPadding,
-                onClick = { id ->
-                    onItemClick(id)
-                }
-            )
-        }
-    )
+    GradientBackground {
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            containerColor = Color.Transparent,
+            topBar = {
+                MainTopBar(
+                    title = stringResource(R.string.populars),
+                    scrollBehavior = scrollBehavior
+                )
+            },
+            content = { innerPadding ->
+                MainContent(
+                    paging = movies,
+                    paddingValues = innerPadding,
+                    onClick = { id ->
+                        onItemClick(id)
+                    }
+                )
+            }
+        )
+    }
 }
--- app/src/main/java/dev/brunofelix/movies/feature/upcoming/presentation/ui/MovieUpcomingScreen.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/upcoming/presentation/ui/MovieUpcomingScreen.kt
@@ -1,35 +1,48 @@
 package dev.brunofelix.movies.feature.upcoming.presentation.ui
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.paging.compose.collectAsLazyPagingItems
 import dev.brunofelix.movies.R
+import dev.brunofelix.movies.core.presentation.components.GradientBackground
 import dev.brunofelix.movies.core.presentation.components.MainContent
 import dev.brunofelix.movies.core.presentation.components.MainTopBar
 import dev.brunofelix.movies.feature.upcoming.presentation.state.MovieUpcomingUiState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieUpcomingScreen(
     uiState: MovieUpcomingUiState,
     onItemClick: (id: Long) -> Unit
 ) {
     val movies = uiState.movies.collectAsLazyPagingItems()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
-    Scaffold(
-        topBar = {
-            MainTopBar(
-                title = stringResource(R.string.upcoming)
-            )
-        },
-        content = { innerPadding ->
-            MainContent(
-                paging = movies,
-                paddingValues = innerPadding,
-                onClick = { id ->
-                    onItemClick(id)
-                }
-            )
-        }
-    )
+    GradientBackground {
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            containerColor = Color.Transparent,
+            topBar = {
+                MainTopBar(
+                    title = stringResource(R.string.upcoming),
+                    scrollBehavior = scrollBehavior
+                )
+            },
+            content = { innerPadding ->
+                MainContent(
+                    paging = movies,
+                    paddingValues = innerPadding,
+                    onClick = { id ->
+                        onItemClick(id)
+                    }
+                )
+            }
+        )
+    }
 }
