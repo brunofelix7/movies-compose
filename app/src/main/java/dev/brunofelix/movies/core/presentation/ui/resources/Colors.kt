57bba305053203afca5af4028fd9756fb69b2a8e
Bruno Felix <brunofelix.dev@gmail.com>
2026-06-22 22:41:21 -0300

    💄 Adding darkGray color to resources

--- app/src/main/java/dev/brunofelix/movies/core/presentation/ui/resources/Colors.kt
+++ app/src/main/java/dev/brunofelix/movies/core/presentation/ui/resources/Colors.kt
@@ -7,5 +7,6 @@ object Colors {
     val white = Color(0xFFFFFFFF)
     val redPrimary = Color(0xFFED202C)
     val lightGray = Color(0xFFA8A8A8)
+    val darkGray = Color(0xFF1E1D1D)
     val darkRed = Color(0xFF1D0103)
 }
