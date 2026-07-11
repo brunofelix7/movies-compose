1560c6f9a2a6c7c9f309f752fc57efa2e397863a
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-10 23:58:50 -0300

    ⬆️ Upgrading Java compatibility and Kotlin toolchain to version 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
@@ -57,7 +57,7 @@ android {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 // Configuration to share dependencies between Unit and Instrumentation tests
