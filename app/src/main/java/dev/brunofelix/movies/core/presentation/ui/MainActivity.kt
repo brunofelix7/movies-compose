675632bd25d0fd04ad0bb1c19f669d2d782d20dd
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-13 22:58:40 -0300

    🎨 Configuring system bar styles for edge-to-edge in MainActivity

--- app/src/main/java/dev/brunofelix/movies/core/presentation/ui/MainActivity.kt
+++ app/src/main/java/dev/brunofelix/movies/core/presentation/ui/MainActivity.kt
@@ -1,10 +1,16 @@
 package dev.brunofelix.movies.core.presentation.ui
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.brunofelix.movies.core.presentation.ui.resources.PMovieTheme
@@ -15,8 +21,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.dark(Color.Transparent.hashCode()),
+            statusBarStyle = SystemBarStyle.dark(Color.Transparent.hashCode())
+        )
         setContent {
+            val view = LocalView.current
+            SideEffect {
+                val window = (view.context as Activity).window
+                val controller = WindowInsetsControllerCompat(window, view)
+                // Status Bar icons colors (false for light icons, true for dark icons)
+                controller.isAppearanceLightStatusBars = false
+
+                // Navigation Bar icons colors (true for dark icons, false for light icons)
+                controller.isAppearanceLightNavigationBars = false
+            }
             PMovieTheme {
                 MainScreen(navController = rememberNavController())
             }
