4a9d6d9b289201ca74a495c633360d34cb67d549
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-15 22:58:20 -0300

    💄 Refactoring DetailContent UI and using MovieGenderContainer component

--- app/src/main/java/dev/brunofelix/movies/feature/detail/presentation/ui/components/DetailContent.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/detail/presentation/ui/components/DetailContent.kt
@@ -1,7 +1,6 @@
 package dev.brunofelix.movies.feature.detail.presentation.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Movie
@@ -18,7 +16,6 @@ import androidx.compose.material.icons.outlined.CalendarMonth
 import androidx.compose.material.icons.outlined.Timer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -26,12 +23,14 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import dev.brunofelix.movies.core.domain.model.MovieGenre
 import dev.brunofelix.movies.core.presentation.state.MovieUiState
 import dev.brunofelix.movies.core.presentation.state.UiState
 import dev.brunofelix.movies.core.presentation.ui.components.CustomButton
 import dev.brunofelix.movies.core.presentation.ui.components.ErrorLayout
 import dev.brunofelix.movies.core.presentation.ui.components.GradientBackground
 import dev.brunofelix.movies.core.presentation.ui.components.LoadingState
+import dev.brunofelix.movies.core.presentation.ui.components.MovieGenderContainer
 import dev.brunofelix.movies.core.presentation.ui.components.MovieInfoChip
 import dev.brunofelix.movies.core.presentation.ui.theme.Colors
 import dev.brunofelix.movies.core.util.extension.formatDecimal
@@ -51,14 +50,7 @@ fun DetailContent(
                 .padding(16.dp)
         ) {
             when (state.uiState) {
-                is UiState.Loading -> {
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        LoadingState()
-                    }
-                }
+                is UiState.Loading -> LoadingState()
                 is UiState.Success -> {
                     LazyColumn {
                         item {
@@ -96,22 +88,13 @@ fun DetailContent(
                                                 text = "${if (state.uiState.data.duration <= 0) "--" else state.uiState.data.duration}min"
                                             )
                                         }
-                                        Spacer(Modifier.size(8.dp))
-                                        Row(
-                                            modifier = Modifier.fillMaxWidth()
+                                        Column(
+                                            modifier = Modifier.padding(vertical = 12.dp)
                                         ) {
-                                            state.uiState.data.genres.forEach { category ->
-                                                Text(
-                                                    text = category.name,
-                                                    fontSize = 14.sp,
-                                                    color = Colors.lightGray,
-                                                    fontWeight = FontWeight.SemiBold,
-                                                    modifier = Modifier.padding(bottom = 8.dp)
-                                                )
-                                                Spacer(Modifier.size(8.dp))
-                                            }
+                                            MovieGenderContainer(
+                                                gendersList = state.uiState.data.genres
+                                            )
                                         }
-                                        Spacer(Modifier.size(8.dp))
                                     }
                                 }
                             }
@@ -180,6 +163,13 @@ private fun SuccessPreview() {
                     duration = 120,
                     voteAverage = 7.5F,
                     overview = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    genres = listOf(
+                        MovieGenre(name = "Action"),
+                        MovieGenre(name = "Adventure"),
+                        MovieGenre(name = "Comedy"),
+                        MovieGenre(name = "Drama"),
+                        MovieGenre(name = "Terror")
+                    )
                 )
             )
         ),
