0acda0de1de1fb9b5a48537908254ac6a1c7013b
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-18 11:09:58 -0300

    💄 Refactoring MainScreen to use type-safe navigation and handle Scaffold padding

--- app/src/main/java/dev/brunofelix/movies/core/presentation/ui/MainScreen.kt
+++ app/src/main/java/dev/brunofelix/movies/core/presentation/ui/MainScreen.kt
@@ -1,36 +1,43 @@
 package dev.brunofelix.movies.core.presentation.ui
 
-import android.annotation.SuppressLint
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import dev.brunofelix.movies.core.presentation.ui.components.CustomNavBar
+import dev.brunofelix.movies.core.presentation.navigation.MovieDestination
 import dev.brunofelix.movies.core.presentation.navigation.MovieNavHost
-import dev.brunofelix.movies.core.presentation.navigation.currentRoute
+import dev.brunofelix.movies.core.presentation.ui.components.CustomNavBar
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MainScreen(navController: NavHostController) {
+fun MainScreen(
+    navController: NavHostController
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val showBottomBar = navBackStackEntry?.destination?.hasRoute<MovieDestination.Details>()
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
-            if (currentRoute(navController)?.contains("DetailsScreen") == false) {
+            if (showBottomBar == false) {
                 CustomNavBar(navController)
             }
         },
-        content = {
-            MovieNavHost(navController)
+        content = { innerPadding ->
+            MovieNavHost(innerPadding, navController)
         }
     )
 }
 
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true)
 @Composable
-private fun MainScreenPreview() {
-    MainScreen(navController = rememberNavController())
+private fun Preview() {
+    MainScreen(
+        navController = rememberNavController()
+    )
 }
