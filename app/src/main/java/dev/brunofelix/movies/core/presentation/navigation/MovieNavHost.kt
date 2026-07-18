6d02f61475fbcb9cff48cb9e2ef567682e76ee49
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-18 10:59:17 -0300

    🔥 Removing unused currentRoute function

--- app/src/main/java/dev/brunofelix/movies/core/presentation/navigation/MovieNavHost.kt
+++ app/src/main/java/dev/brunofelix/movies/core/presentation/navigation/MovieNavHost.kt
@@ -1,10 +1,8 @@
 package dev.brunofelix.movies.core.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.currentBackStackEntryAsState
 import dev.brunofelix.movies.feature.detail.presentation.navigation.movieDetailGraph
 import dev.brunofelix.movies.feature.favorite.presentation.navigation.favoriteNavGraph
 import dev.brunofelix.movies.feature.popular.presentation.navigation.popularNavGraph
@@ -38,10 +36,4 @@ fun MovieNavHost(
             navController = navController
         )
     }
-}
-
-@Composable
-fun currentRoute(navController: NavHostController): String? {
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    return navBackStackEntry?.destination?.route
 }
