5d6ecce52104505f1ac1a2e73d42045f142d776a
Bruno Felix <brunofelix.dev@gmail.com>
2026-07-20 12:11:30 -0300

    ♻️ Refactoring MovieFavoriteModule to use @Binds for GetFavoriteMoviesUseCase

--- app/src/main/java/dev/brunofelix/movies/feature/favorite/di/MovieFavoriteModule.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/favorite/di/MovieFavoriteModule.kt
@@ -1,23 +1,18 @@
 package dev.brunofelix.movies.feature.favorite.di
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dev.brunofelix.movies.core.domain.repository.MovieRepository
 import dev.brunofelix.movies.feature.favorite.domain.use_case.GetFavoriteMoviesUseCase
 import dev.brunofelix.movies.feature.favorite.domain.use_case.GetFavoriteMoviesUseCaseImpl
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object FavoriteModule {
+abstract class MovieFavoriteModule {
 
-    @Provides
-    @Singleton
-    fun provideGetFavoriteMoviesUseCase(
-        repository: MovieRepository
-    ): GetFavoriteMoviesUseCase {
-        return GetFavoriteMoviesUseCaseImpl(repository)
-    }
+    @Binds
+    abstract fun bindGetFavoriteMoviesUseCase(
+        impl: GetFavoriteMoviesUseCaseImpl
+    ): GetFavoriteMoviesUseCase
 }
