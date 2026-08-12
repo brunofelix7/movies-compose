f24c3fc3089b2226737d9b1c438dc8a267f06bfc
bruno <brunofelix.dev@gmail.com>
2026-08-12 20:35:47 -0300

    💄 Adding Scaffold and MainTopBar to MovieSearchScreen

--- app/src/main/java/dev/brunofelix/movies/feature/search/presentation/ui/MovieSearchScreen.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/search/presentation/ui/MovieSearchScreen.kt
@@ -4,6 +4,9 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -14,43 +17,63 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import dev.brunofelix.movies.R
 import dev.brunofelix.movies.core.presentation.ui.components.CustomSearchBar
 import dev.brunofelix.movies.core.presentation.ui.components.GradientBackground
+import dev.brunofelix.movies.core.presentation.ui.components.MainTopBar
 import dev.brunofelix.movies.core.presentation.ui.theme.PMovieTheme
 import dev.brunofelix.movies.core.util.logInfo
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieSearchScreen(
     modifier: Modifier = Modifier
 ) {
     var searchQuery by remember { mutableStateOf("") }
     val focusRequester = remember { FocusRequester() }
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     LaunchedEffect(Unit) {
         focusRequester.requestFocus()
     }
 
     GradientBackground {
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(16.dp)
-        ) {
-            CustomSearchBar(
-                query = searchQuery,
-                placeholderText = "Search movie",
-                onQueryChange = { newQuery ->
-                    searchQuery = newQuery
-                },
-                onSearch = { submittedQuery ->
-                    logInfo("query: $submittedQuery")
-                },
-                modifier = Modifier.focusRequester(focusRequester),
-                containerColor = Color.Black.copy(alpha = 0.25F)
-            )
-        }
+        Scaffold(
+            modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            containerColor = Color.Transparent,
+            topBar = {
+                MainTopBar(
+                    title = stringResource(R.string.search),
+                    scrollBehavior = scrollBehavior
+                )
+            },
+            content = { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                ) {
+                    CustomSearchBar(
+                        query = searchQuery,
+                        placeholderText = "Search movie",
+                        onQueryChange = { newQuery ->
+                            searchQuery = newQuery
+                        },
+                        onSearch = { submittedQuery ->
+                            logInfo("query: $submittedQuery")
+                        },
+                        containerColor = Color.Black.copy(alpha = 0.25F),
+                        modifier = Modifier
+                            .focusRequester(focusRequester)
+                            .padding(horizontal = 12.dp)
+                    )
+                }
+            }
+        )
     }
 }
 
