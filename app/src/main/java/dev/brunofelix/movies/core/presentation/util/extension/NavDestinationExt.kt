4509df164df3da46bde1e335b9365a5f08b7ccaf
bruno <brunofelix.dev@gmail.com>
2026-08-12 20:36:26 -0300

    🔧 Updating bottom bar visibility logic to use AppDestination routes

--- app/src/main/java/dev/brunofelix/movies/core/presentation/util/extension/NavDestinationExt.kt
+++ app/src/main/java/dev/brunofelix/movies/core/presentation/util/extension/NavDestinationExt.kt
@@ -2,14 +2,15 @@ package dev.brunofelix.movies.core.presentation.util.extension
 
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hasRoute
-import dev.brunofelix.movies.core.presentation.navigation.MovieDestination
+import dev.brunofelix.movies.core.presentation.navigation.AppDestination
 
 val NavDestination?.shouldShowBottomBar: Boolean
     get() = this?.let { dest ->
         val topLevelRoutes = listOf(
-            MovieDestination.Populars::class,
-            MovieDestination.Upcoming::class,
-            MovieDestination.Favorites::class,
+            AppDestination.Movies::class,
+            AppDestination.TvShows::class,
+            AppDestination.Search::class,
+            AppDestination.Favorites::class,
         )
         topLevelRoutes.any { route -> dest.hasRoute(route) }
     } ?: false
