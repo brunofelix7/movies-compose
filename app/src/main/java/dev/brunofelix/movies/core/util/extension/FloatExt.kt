ee41768a185b28125a263d07090898deccea968f
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-13 12:36:23 -0300

    ♻️ Refactoring Extensions.kt into type-specific FloatExt and StringExt files

---
+++ app/src/main/java/dev/brunofelix/movies/core/util/extension/FloatExt.kt
@@ -0,0 +1,3 @@
+package dev.brunofelix.movies.core.util.extension
+
+fun Float.formatDecimal() = "%.1f".format(this)
--- app/src/main/java/dev/brunofelix/movies/core/util/extension/StringExt.kt
+++ app/src/main/java/dev/brunofelix/movies/core/util/extension/StringExt.kt
@@ -5,5 +5,3 @@ import dev.brunofelix.movies.BuildConfig
 fun String?.toPostUrl() = "${BuildConfig.BASE_URL_IMAGE}$this"
 
 fun String?.toBackdropUrl() = "${BuildConfig.BASE_URL_IMAGE}$this"
-
-fun Float.formatDecimal() = "%.1f".format(this)
