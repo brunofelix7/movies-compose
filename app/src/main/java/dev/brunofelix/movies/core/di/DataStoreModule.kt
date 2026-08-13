485237a9e8779adb73d53bb6c66cf5d631485cc4
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-13 14:48:56 -0300

    ✨ Adding DataStoreModule to provide DataStore and bind LanguageRepository

---
+++ app/src/main/java/dev/brunofelix/movies/core/di/DataStoreModule.kt
@@ -0,0 +1,41 @@
+package dev.brunofelix.movies.core.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.brunofelix.movies.core.data.repository.LanguageRepositoryImpl
+import dev.brunofelix.movies.core.domain.repository.LanguageRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataStoreModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLanguageRepository(
+        impl: LanguageRepositoryImpl
+    ): LanguageRepository
+
+    companion object {
+        private const val DATASTORE_NAME = "settings"
+
+        @Provides
+        @Singleton
+        fun providePreferencesDataStore(
+            @ApplicationContext context: Context
+        ): DataStore<Preferences> {
+            return PreferenceDataStoreFactory.create(
+                produceFile = { context.preferencesDataStoreFile(DATASTORE_NAME) }
+            )
+        }
+    }
+}
