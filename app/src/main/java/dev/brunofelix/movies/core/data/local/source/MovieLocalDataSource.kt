0ac3ef6a0c7434005fcc4c07820cf806d4fbc0c0
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-12 22:07:05 -0300

    ♻️ Refactoring by moving MovieLocalDataSourceImpl into MovieLocalDataSource.kt

--- app/src/main/java/dev/brunofelix/movies/core/data/local/source/MovieLocalDataSource.kt
+++ app/src/main/java/dev/brunofelix/movies/core/data/local/source/MovieLocalDataSource.kt
@@ -1,14 +1,39 @@
 package dev.brunofelix.movies.core.data.local.source
 
+import dev.brunofelix.movies.core.data.local.db.dao.MovieDao
+import dev.brunofelix.movies.core.data.local.db.mapper.toDomain
+import dev.brunofelix.movies.core.data.local.db.mapper.toEntity
 import dev.brunofelix.movies.core.domain.model.Movie
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
 interface MovieLocalDataSource {
     suspend fun insert(movie: Movie): Long
-
     suspend fun delete(movie: Movie): Int
-
     suspend fun getById(id: Long): Movie?
-
     fun getAll(): Flow<List<Movie>>
+}
+
+class MovieLocalDataSourceImpl @Inject constructor(
+    private val dao: MovieDao
+): MovieLocalDataSource {
+
+    override suspend fun insert(movie: Movie): Long {
+        return dao.insert(movie.toEntity())
+    }
+
+    override suspend fun delete(movie: Movie): Int {
+        return dao.delete(movie.toEntity())
+    }
+
+    override suspend fun getById(id: Long): Movie? {
+        return dao.getById(id)?.toDomain()
+    }
+
+    override fun getAll(): Flow<List<Movie>> {
+        return dao.getAll().map { entityList ->
+            entityList.map { it.toDomain() }
+        }
+    }
 }
