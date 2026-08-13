b1aedb59dd8f1bfbea934fcc6c08ad4cd112bcf2
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-13 11:18:54 -0300

    💡 Adding toDomainList extensions and KDoc to movie and TV show mappers

--- app/src/main/java/dev/brunofelix/movies/core/data/remote/mapper/MovieDtoMapper.kt
+++ app/src/main/java/dev/brunofelix/movies/core/data/remote/mapper/MovieDtoMapper.kt
@@ -1,10 +1,23 @@
 package dev.brunofelix.movies.core.data.remote.mapper
 
 import dev.brunofelix.movies.core.data.remote.dto.movie.MovieDto
+import dev.brunofelix.movies.core.data.remote.dto.movie.MovieRootDto
 import dev.brunofelix.movies.core.domain.model.Movie
 import dev.brunofelix.movies.core.util.extension.toBackdropUrl
 import dev.brunofelix.movies.core.util.extension.toPostUrl
 
+/**
+ * Extension function to map a [MovieRootDto] to a list of [Movie] domain models.
+ * @return A list of [Movie]s or an empty list if results are null.
+ */
+fun MovieRootDto.toDomainList(): List<Movie> {
+    return results?.map { it.toDomain() } ?: emptyList()
+}
+
+/**
+ * Extension function to map a [MovieDto] to a [Movie] domain model.
+ * @return A domain representation of the movie.
+ */
 fun MovieDto.toDomain(): Movie {
     return Movie(
         id = id ?: -1L,
--- app/src/main/java/dev/brunofelix/movies/core/data/remote/mapper/TvShowDtoMapper.kt
+++ app/src/main/java/dev/brunofelix/movies/core/data/remote/mapper/TvShowDtoMapper.kt
@@ -1,8 +1,21 @@
 package dev.brunofelix.movies.core.data.remote.mapper
 
 import dev.brunofelix.movies.core.data.remote.dto.tv_show.TvShowDto
+import dev.brunofelix.movies.core.data.remote.dto.tv_show.TvShowRootDto
 import dev.brunofelix.movies.core.domain.model.TvShow
 
+/**
+ * Extension function to map a [TvShowRootDto] to a list of [TvShow] domain models.
+ * @return A list of [TvShow]s or an empty list if results are null.
+ */
+fun TvShowRootDto.toDomainList(): List<TvShow> {
+    return results?.map { it.toDomain() } ?: emptyList()
+}
+
+/**
+ * Extension function to map a [TvShowDto] to a [TvShow] domain model.
+ * @return A domain representation of the TV show.
+ */
 fun TvShowDto.toDomain(): TvShow {
     return TvShow(
         id = id ?: -1L,
