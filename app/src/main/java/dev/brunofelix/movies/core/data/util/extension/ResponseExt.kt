d9e0a3125f3d16e326ed2bb2f26170a62ea14358
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-13 13:11:17 -0300

    🔧 Adding toResult extension and updating error mapping in ResponseExt.kt

--- app/src/main/java/dev/brunofelix/movies/core/data/util/extension/ResponseExt.kt
+++ app/src/main/java/dev/brunofelix/movies/core/data/util/extension/ResponseExt.kt
@@ -7,6 +7,23 @@ inline fun <T, R> Response<T>.mapOrThrow(transform: (T) -> R): R {
     if (isSuccessful) {
         return body()?.let(transform) ?: throw NullPointerException("Response body is null")
     } else {
-        throw HttpException(this)
+        throw HttpException(this).toRemoteException()
+    }
+}
+
+fun <T> Response<T>.toResult(): Result<T> {
+    return try {
+        if (isSuccessful) {
+            val body = body()
+            if (body != null) {
+                Result.success(body)
+            } else {
+                Result.failure(NullPointerException("Response body is null"))
+            }
+        } else {
+            Result.failure(HttpException(this).toRemoteException())
+        }
+    } catch (e: Exception) {
+        Result.failure(e.toRemoteException())
     }
 }
