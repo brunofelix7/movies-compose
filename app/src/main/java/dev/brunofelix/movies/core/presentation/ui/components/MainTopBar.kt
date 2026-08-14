1ec83f339edadf2735162506fab5a1eeb35eb0eb
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-13 22:26:42 -0300

    💄 Refactoring MainTopBar to include static multi-line title and settings callback

--- app/src/main/java/dev/brunofelix/movies/core/presentation/ui/components/MainTopBar.kt
+++ app/src/main/java/dev/brunofelix/movies/core/presentation/ui/components/MainTopBar.kt
@@ -1,10 +1,10 @@
 package dev.brunofelix.movies.core.presentation.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -26,18 +26,25 @@ import dev.brunofelix.movies.core.presentation.ui.theme.Colors
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainTopBar(
-    title: String,
     modifier: Modifier = Modifier,
-    scrollBehavior: TopAppBarScrollBehavior? = null
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    onSettings: () -> Unit = {}
 ) {
     TopAppBar(
         scrollBehavior = scrollBehavior,
         title = {
-            Text(
-                text = title,
-                color = Colors.white,
-                style = MaterialTheme.typography.titleLarge
-            )
+            Column {
+                Text(
+                    text = stringResource(R.string.movies),
+                    color = Colors.white,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Text(
+                    text = stringResource(R.string.compose),
+                    color = Colors.lightGray,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         },
         windowInsets = TopAppBarDefaults.windowInsets.add(WindowInsets(top = 8.dp)),
         colors = TopAppBarDefaults.topAppBarColors(
@@ -56,7 +63,7 @@ fun MainTopBar(
                         contentDescription = stringResource(R.string.top_bar_language_icon)
                     )
                 },
-                onClick = {}
+                onClick = onSettings
             )
         },
         modifier = modifier.fillMaxWidth()
@@ -67,8 +74,5 @@ fun MainTopBar(
 @Preview
 @Composable
 private fun Preview() {
-    MainTopBar(
-        title = stringResource(R.string.upcoming),
-        scrollBehavior = null
-    )
+    MainTopBar()
 }
