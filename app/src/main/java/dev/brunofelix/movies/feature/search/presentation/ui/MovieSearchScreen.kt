61665e1001b189f7654b82038e5f5b2ad15f2b62
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-14 15:50:02 -0300

    💄 Clearing focus on tap in MovieSearchScreen

--- app/src/main/java/dev/brunofelix/movies/feature/search/presentation/ui/MovieSearchScreen.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/search/presentation/ui/MovieSearchScreen.kt
@@ -1,6 +1,7 @@
 package dev.brunofelix.movies.feature.search.presentation.ui
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,12 +28,18 @@ fun MovieSearchScreen(
     modifier: Modifier = Modifier,
     paddingValues: PaddingValues = PaddingValues()
 ) {
+    val focusManager = LocalFocusManager.current
     var searchQuery by remember { mutableStateOf("") }
 
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(paddingValues)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            }
     ) {
         CustomSearchBar(
             query = searchQuery,
