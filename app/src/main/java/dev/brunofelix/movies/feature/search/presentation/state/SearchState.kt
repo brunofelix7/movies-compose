70a9c36fe9e20fc1c94f267d69ce4e1261738220
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-16 10:22:17 -0300

    ♻️ refactor(search): update SearchState with isLoading property and refining searchResults default

--- app/src/main/java/dev/brunofelix/movies/feature/search/presentation/state/SearchState.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/search/presentation/state/SearchState.kt
@@ -3,9 +3,10 @@ package dev.brunofelix.movies.feature.search.presentation.state
 import androidx.paging.PagingData
 import dev.brunofelix.movies.core.domain.model.Media
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 
 data class SearchState(
     val query: String = "",
-    val searchResults: Flow<PagingData<Media>> = emptyFlow()
+    val searchResults: Flow<PagingData<Media>> = flowOf(PagingData.empty()),
+    val isLoading: Boolean = false
 )
