690d5fb0430b99dabd419eb1b590f363857fcc01
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-16 00:09:44 -0300

    ✨ feat(search): add MediaMapper in domain to convert search results to Media

---
+++ app/src/main/java/dev/brunofelix/movies/core/domain/mapper/MediaMapper.kt
@@ -0,0 +1,30 @@
+package dev.brunofelix.movies.core.domain.mapper
+
+import dev.brunofelix.movies.core.domain.model.Media
+import dev.brunofelix.movies.core.domain.model.Movie
+import dev.brunofelix.movies.core.domain.model.TvShow
+import dev.brunofelix.movies.core.domain.model.enums.MediaType
+
+fun Movie.toMedia() = Media(
+    id = id,
+    title = title,
+    posterPath = posterPath,
+    voteAverage = voteAverage,
+    releaseDate = releaseDate,
+    overview = overview,
+    type = MediaType.MOVIE
+)
+
+fun List<Movie>.toMovieMediaList() = map { it.toMedia() }
+
+fun TvShow.toMedia() = Media(
+    id = id,
+    title = name,
+    posterPath = posterPath,
+    voteAverage = voteAverage,
+    releaseDate = firstAirDate,
+    overview = overview,
+    type = MediaType.TV_SHOW
+)
+
+fun List<TvShow>.toTvShowMediaList() = map { it.toMedia() }
