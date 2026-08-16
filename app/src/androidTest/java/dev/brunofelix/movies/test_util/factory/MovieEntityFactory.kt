abf430a0120ba2bfe1f664c1f76325d0bb082f9f
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-16 14:46:08 -0300

    ♻️ refactor(test): update MovieEntityFactory to include type property in MediaEntity

--- app/src/androidTest/java/dev/brunofelix/movies/test_util/factory/MovieEntityFactory.kt
+++ app/src/androidTest/java/dev/brunofelix/movies/test_util/factory/MovieEntityFactory.kt
@@ -1,20 +1,69 @@
 package dev.brunofelix.movies.test_util.factory
 
 import dev.brunofelix.movies.core.data.local.db.entity.MediaEntity
+import dev.brunofelix.movies.core.domain.model.enums.MediaType
 
 object MovieEntityFactory {
 
     fun create(id: Long): MediaEntity {
-        return MediaEntity(id = id, title = "Movie $id", posterPath = "URL $id", voteAverage = 1F, duration = 1, releaseDate = "")
+        return MediaEntity(
+            id = id,
+            title = "Movie $id",
+            posterPath = "URL $id",
+            voteAverage = 1F,
+            duration = 1,
+            releaseDate = "",
+            type = MediaType.MOVIE
+        )
     }
 
     fun createList(): List<MediaEntity> {
         return listOf(
-            MediaEntity(id = 2, title = "Movie 5", posterPath = "URL 2", voteAverage = 1F, duration = 1, releaseDate = ""),
-            MediaEntity(id = 5, title = "Movie 3", posterPath = "URL 5", voteAverage = 1F, duration = 1, releaseDate = ""),
-            MediaEntity(id = 1, title = "Movie 4", posterPath = "URL 1", voteAverage = 1F, duration = 1, releaseDate = ""),
-            MediaEntity(id = 4, title = "Movie 2", posterPath = "URL 4", voteAverage = 1F, duration = 1, releaseDate = ""),
-            MediaEntity(id = 3, title = "Movie 1", posterPath = "URL 3", voteAverage = 1F, duration = 1, releaseDate = "")
+            MediaEntity(
+                id = 2,
+                title = "Movie 5",
+                posterPath = "URL 2",
+                voteAverage = 1F,
+                duration = 1,
+                releaseDate = "",
+                type = MediaType.MOVIE
+            ),
+            MediaEntity(
+                id = 5,
+                title = "Movie 3",
+                posterPath = "URL 5",
+                voteAverage = 1F,
+                duration = 1,
+                releaseDate = "",
+                type = MediaType.MOVIE
+            ),
+            MediaEntity(
+                id = 1,
+                title = "Movie 4",
+                posterPath = "URL 1",
+                voteAverage = 1F,
+                duration = 1,
+                releaseDate = "",
+                type = MediaType.MOVIE
+            ),
+            MediaEntity(
+                id = 4,
+                title = "Movie 2",
+                posterPath = "URL 4",
+                voteAverage = 1F,
+                duration = 1,
+                releaseDate = "",
+                type = MediaType.MOVIE
+            ),
+            MediaEntity(
+                id = 3,
+                title = "Movie 1",
+                posterPath = "URL 3",
+                voteAverage = 1F,
+                duration = 1,
+                releaseDate = "",
+                type = MediaType.MOVIE
+            )
         )
     }
 }
