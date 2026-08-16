27399b8d1c71478b81a01ebd137c8f1a712b4d5d
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-16 00:38:50 -0300

    ♻️ refactor(search): migrate SearchRepositoryImpl to use RemoteDataSources instead of Services

--- app/src/main/java/dev/brunofelix/movies/feature/search/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/search/data/repository/SearchRepositoryImpl.kt
@@ -3,22 +3,19 @@ package dev.brunofelix.movies.feature.search.data.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import dev.brunofelix.movies.core.data.remote.MovieService
-import dev.brunofelix.movies.core.data.remote.TvShowService
-import dev.brunofelix.movies.core.data.remote.mapper.toDomainList
+import dev.brunofelix.movies.core.data.remote.paging.BasePagingSource
+import dev.brunofelix.movies.core.data.remote.source.MovieRemoteDataSource
+import dev.brunofelix.movies.core.data.remote.source.TvShowRemoteDataSource
 import dev.brunofelix.movies.core.domain.mapper.toMovieMediaList
 import dev.brunofelix.movies.core.domain.mapper.toTvShowMediaList
-import dev.brunofelix.movies.core.data.remote.paging.BasePagingSource
-import dev.brunofelix.movies.core.data.util.extension.mapOrThrow
-import dev.brunofelix.movies.core.data.util.extension.toRemoteException
 import dev.brunofelix.movies.core.domain.model.Media
 import dev.brunofelix.movies.feature.search.domain.repository.SearchRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class SearchRepositoryImpl @Inject constructor(
-    private val movieService: MovieService,
-    private val tvShowService: TvShowService
+    private val movieRemoteDataSource: MovieRemoteDataSource,
+    private val tvShowRemoteDataSource: TvShowRemoteDataSource
 ) : SearchRepository {
 
     override fun search(
@@ -30,16 +27,10 @@ class SearchRepositoryImpl @Inject constructor(
             pagingSourceFactory = {
                 BasePagingSource(pagingConfig.pageSize) { page ->
                     runCatching {
-                        val movieResponse = movieService.search(query, page)
-                        val tvShowResponse = tvShowService.search(query, page)
-                        val movies = movieResponse.mapOrThrow {
-                            it.toDomainList().toMovieMediaList()
-                        }
-                        val tvShows = tvShowResponse.mapOrThrow {
-                            it.toDomainList().toTvShowMediaList()
-                        }
-                        movies + tvShows
-                    }.recoverCatching { throw it.toRemoteException() }
+                        val movies = movieRemoteDataSource.search(query, page).getOrThrow()
+                        val tvShows = tvShowRemoteDataSource.search(query, page).getOrThrow()
+                        movies.toMovieMediaList() + tvShows.toTvShowMediaList()
+                    }
                 }
             }
         ).flow
