b61e5e8f57a767aed5632a8468a9b28f5036cd89
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-16 21:45:04 -0300

    ♻️ refactor(media): add default values and duration to Media model, and remove overview

--- app/src/main/java/dev/brunofelix/movies/core/domain/model/Media.kt
+++ app/src/main/java/dev/brunofelix/movies/core/domain/model/Media.kt
@@ -6,11 +6,11 @@ import dev.brunofelix.movies.core.domain.model.enums.MediaType
  * Represents a media item, such as a movie or TV show.
  */
 data class Media(
-    val id: Long,
-    val title: String,
-    val posterPath: String,
-    val voteAverage: Float,
-    val releaseDate: String,
-    val overview: String,
-    val type: MediaType
+    val id: Long = 0L,
+    val title: String = "",
+    val posterPath: String = "",
+    val voteAverage: Float = 0F,
+    val releaseDate: String = "",
+    val duration: Int = 0,
+    val type: MediaType = MediaType.MOVIE
 )
