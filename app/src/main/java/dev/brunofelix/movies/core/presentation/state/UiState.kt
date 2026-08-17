6648bb69956fef43e07d7fa6068d167b5c42b806
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-17 11:30:38 -0300

    ✨ feat: add Empty state to UiState

--- app/src/main/java/dev/brunofelix/movies/core/presentation/state/UiState.kt
+++ app/src/main/java/dev/brunofelix/movies/core/presentation/state/UiState.kt
@@ -4,6 +4,7 @@ import dev.brunofelix.movies.core.presentation.util.UiText
 
 sealed class UiState<out T> {
     data object Loading : UiState<Nothing>()
+    data object Empty : UiState<Nothing>()
     data class Success<T>(val data: T) : UiState<T>()
     data class Error(val uiText: UiText) : UiState<Nothing>()
 }
