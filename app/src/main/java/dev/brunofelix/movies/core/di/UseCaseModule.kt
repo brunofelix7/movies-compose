8981f09563d1d426782e7a3191d0c1c5fc2cfd8f
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-17 11:07:28 -0300

    ✨ feat(media): add UseCaseModule to bind media use cases

---
+++ app/src/main/java/dev/brunofelix/movies/core/di/UseCaseModule.kt
@@ -0,0 +1,39 @@
+package dev.brunofelix.movies.core.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.brunofelix.movies.core.domain.use_case.DeleteMediaUseCase
+import dev.brunofelix.movies.core.domain.use_case.DeleteMediaUseCaseImpl
+import dev.brunofelix.movies.core.domain.use_case.GetFavoriteMediasUseCase
+import dev.brunofelix.movies.core.domain.use_case.GetFavoriteMediasUseCaseImpl
+import dev.brunofelix.movies.core.domain.use_case.IsFavoriteMediaUseCase
+import dev.brunofelix.movies.core.domain.use_case.IsFavoriteMediaUseCaseImpl
+import dev.brunofelix.movies.core.domain.use_case.SaveMediaUseCase
+import dev.brunofelix.movies.core.domain.use_case.SaveMediaUseCaseImpl
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class UseCaseModule {
+
+    @Binds
+    abstract fun bindGetFavoriteMediasUseCase(
+        impl: GetFavoriteMediasUseCaseImpl
+    ): GetFavoriteMediasUseCase
+
+    @Binds
+    abstract fun bindSaveMovieUseCase(
+        impl: SaveMediaUseCaseImpl
+    ): SaveMediaUseCase
+
+    @Binds
+    abstract fun bindDeleteMediaUseCase(
+        impl: DeleteMediaUseCaseImpl
+    ): DeleteMediaUseCase
+
+    @Binds
+    abstract fun bindIsFavoriteMovieUseCase(
+        impl: IsFavoriteMediaUseCaseImpl
+    ): IsFavoriteMediaUseCase
+}
