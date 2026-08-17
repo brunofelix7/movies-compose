4b96e223104596e0a14838ef6c29820e59a89c33
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-16 21:33:42 -0300

    🔧 chore(database): add TypeConverters to AppDatabase

--- app/src/main/java/dev/brunofelix/movies/core/data/local/db/AppDatabase.kt
+++ app/src/main/java/dev/brunofelix/movies/core/data/local/db/AppDatabase.kt
@@ -2,6 +2,8 @@ package dev.brunofelix.movies.core.data.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import dev.brunofelix.movies.core.data.local.db.converter.Converters
 import dev.brunofelix.movies.core.data.local.db.dao.MediaDao
 import dev.brunofelix.movies.core.data.local.db.entity.MediaEntity
 
@@ -14,6 +16,7 @@ import dev.brunofelix.movies.core.data.local.db.entity.MediaEntity
     exportSchema = false,
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     /**
      * Provides access to [MediaDao] for movie-related database operations.
