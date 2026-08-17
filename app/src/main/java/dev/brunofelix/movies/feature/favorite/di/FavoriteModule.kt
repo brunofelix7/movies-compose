caf111702f83a198ee76c91454f8fc0d8fe08ed9
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-17 11:06:32 -0300

    🔥 remove(media): remove unused use case bindings

--- app/src/main/java/dev/brunofelix/movies/feature/favorite/di/FavoriteModule.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/favorite/di/FavoriteModule.kt
@@ -1,18 +1,9 @@
 package dev.brunofelix.movies.feature.favorite.di
 
-import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dev.brunofelix.movies.feature.favorite.domain.use_case.GetFavoriteMediasUseCase
-import dev.brunofelix.movies.feature.favorite.domain.use_case.GetFavoriteMediasUseCaseImpl
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class FavoriteModule {
-
-    @Binds
-    abstract fun bindGetFavoriteMediasUseCase(
-        impl: GetFavoriteMediasUseCaseImpl
-    ): GetFavoriteMediasUseCase
-}
+abstract class FavoriteModule
--- app/src/main/java/dev/brunofelix/movies/feature/movie/detail/di/MovieDetailModule.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/movie/detail/di/MovieDetailModule.kt
@@ -4,14 +4,8 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dev.brunofelix.movies.feature.movie.detail.domain.use_case.DeleteMovieUseCase
-import dev.brunofelix.movies.feature.movie.detail.domain.use_case.DeleteMovieUseCaseImpl
-import dev.brunofelix.movies.feature.movie.detail.domain.use_case.GetMovieDetailsUseCase
-import dev.brunofelix.movies.feature.movie.detail.domain.use_case.GetMovieDetailsUseCaseImpl
-import dev.brunofelix.movies.feature.movie.detail.domain.use_case.IsFavoriteMovieUseCase
-import dev.brunofelix.movies.feature.movie.detail.domain.use_case.IsFavoriteMovieUseCaseImpl
-import dev.brunofelix.movies.feature.movie.detail.domain.use_case.SaveMovieUseCase
-import dev.brunofelix.movies.feature.movie.detail.domain.use_case.SaveMovieUseCaseImpl
+import dev.brunofelix.movies.feature.movie.detail.domain.use_case.GetMovieDetailUseCase
+import dev.brunofelix.movies.feature.movie.detail.domain.use_case.GetMovieDetailUseCaseImpl
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -19,21 +13,6 @@ abstract class MovieDetailModule {
 
     @Binds
     abstract fun bindGetMovieDetailsUseCase(
-        impl: GetMovieDetailsUseCaseImpl
-    ): GetMovieDetailsUseCase
-
-    @Binds
-    abstract fun bindSaveMovieUseCase(
-        impl: SaveMovieUseCaseImpl
-    ): SaveMovieUseCase
-
-    @Binds
-    abstract fun bindDeleteMovieUseCase(
-        impl: DeleteMovieUseCaseImpl
-    ): DeleteMovieUseCase
-
-    @Binds
-    abstract fun bindIsFavoriteMovieUseCase(
-        impl: IsFavoriteMovieUseCaseImpl
-    ): IsFavoriteMovieUseCase
+        impl: GetMovieDetailUseCaseImpl
+    ): GetMovieDetailUseCase
 }
