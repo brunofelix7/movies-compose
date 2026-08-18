a66b5e8502e328520cea08c0a0ed34af40793cd4
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-17 21:28:26 -0300

    ✨ feat(movie): add MovieUiModel to the presentation layer

---
+++ app/src/main/java/dev/brunofelix/movies/core/presentation/ui/model/MovieUiModel.kt
@@ -0,0 +1,15 @@
+package dev.brunofelix.movies.core.presentation.ui.model
+
+import dev.brunofelix.movies.core.domain.model.MovieGenre
+
+data class MovieUiModel(
+    val id: Long = 0L,
+    val title: String = "",
+    val overview: String = "",
+    val posterPath: String = "",
+    val backdropPath: String = "",
+    val releaseDate: String = "",
+    val voteAverage: String = "",
+    val duration: String = "",
+    val genres: List<MovieGenre> = emptyList()
+)
