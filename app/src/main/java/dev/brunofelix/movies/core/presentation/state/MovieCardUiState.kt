dd54a8801f91ce513b1526d5338fca2c5767985b
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-17 21:09:58 -0300

    🔥 remove(movie): remove MovieCardUiState, MovieDetailState, and MovieUiState classes

--- app/src/main/java/dev/brunofelix/movies/core/presentation/state/MovieCardUiState.kt
+++
@@ -1,7 +0,0 @@
-package dev.brunofelix.movies.core.presentation.state
-
-sealed class MovieCardUiState {
-    data object Loading : MovieCardUiState()
-    data object Success : MovieCardUiState()
-    data object Error : MovieCardUiState()
-}
