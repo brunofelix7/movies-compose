28ecdea1a1a23cb73be7de5be46ccd9d15436496
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-18 14:02:12 -0300

    🚚 refactor(favorite): move GetFavoriteMediasUseCase binding from core to favorite module

--- app/src/main/java/dev/brunofelix/movies/core/di/UseCaseModule.kt
+++ app/src/main/java/dev/brunofelix/movies/core/di/UseCaseModule.kt
@@ -6,8 +6,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.brunofelix.movies.core.domain.use_case.DeleteMediaUseCase
 import dev.brunofelix.movies.core.domain.use_case.DeleteMediaUseCaseImpl
-import dev.brunofelix.movies.core.domain.use_case.GetFavoriteMediasUseCase
-import dev.brunofelix.movies.core.domain.use_case.GetFavoriteMediasUseCaseImpl
 import dev.brunofelix.movies.core.domain.use_case.IsFavoriteMediaUseCase
 import dev.brunofelix.movies.core.domain.use_case.IsFavoriteMediaUseCaseImpl
 import dev.brunofelix.movies.core.domain.use_case.SaveMediaUseCase
@@ -16,11 +14,6 @@ import dev.brunofelix.movies.core.domain.use_case.SaveMediaUseCaseImpl
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class UseCaseModule {
-
-    @Binds
-    abstract fun bindGetFavoriteMediasUseCase(
-        impl: GetFavoriteMediasUseCaseImpl
-    ): GetFavoriteMediasUseCase
 
     @Binds
     abstract fun bindSaveMovieUseCase(
