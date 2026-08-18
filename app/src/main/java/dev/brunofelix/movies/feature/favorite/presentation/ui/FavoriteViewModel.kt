32bd01b7087433d65bd87aeb9b9e76db88be5ff3
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-18 13:55:16 -0300

    🚚 refactor(favorite): move FavoriteViewModel to ui package

--- app/src/main/java/dev/brunofelix/movies/feature/favorite/presentation/ui/FavoriteViewModel.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/favorite/presentation/ui/FavoriteViewModel.kt
@@ -1,12 +1,12 @@
-package dev.brunofelix.movies.feature.favorite.presentation.viewmodel
+package dev.brunofelix.movies.feature.favorite.presentation.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.brunofelix.movies.core.data.util.extension.toUiText
 import dev.brunofelix.movies.core.domain.model.Media
-import dev.brunofelix.movies.core.domain.use_case.GetFavoriteMediasUseCase
 import dev.brunofelix.movies.core.presentation.util.UiState
+import dev.brunofelix.movies.feature.favorite.domain.use_case.GetFavoriteMediasUseCase
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
