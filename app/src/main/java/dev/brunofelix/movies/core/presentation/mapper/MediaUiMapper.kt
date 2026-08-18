bc78e14f286babacfd35a71abb94d81397270aef
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-18 18:02:57 -0300

    🔧 refactor(media): format release date, vote average, and duration in MediaUiMapper

--- app/src/main/java/dev/brunofelix/movies/core/presentation/mapper/MediaUiMapper.kt
+++ app/src/main/java/dev/brunofelix/movies/core/presentation/mapper/MediaUiMapper.kt
@@ -1,6 +1,8 @@
 package dev.brunofelix.movies.core.presentation.mapper
 
 import dev.brunofelix.movies.core.domain.model.Media
+import dev.brunofelix.movies.core.domain.util.datetime.DateTimeConverter
+import dev.brunofelix.movies.core.domain.util.extension.formatDecimal
 import dev.brunofelix.movies.core.presentation.ui.model.MediaUiModel
 import dev.brunofelix.movies.core.presentation.ui.model.MovieUiModel
 
@@ -13,5 +15,12 @@ fun Media.toUiModel() = MediaUiModel(
     id = id,
     title = title,
     posterPath = posterPath,
+    releaseDate = DateTimeConverter.format(
+        value = releaseDate,
+        fromPattern = DateTimeConverter.YYYY_MM_DD,
+        toPattern = DateTimeConverter.DD_MM_YYYY
+    ).value,
+    voteAverage = if (voteAverage <= 0) "--" else voteAverage.formatDecimal(),
+    duration = "${if (duration <= 0) "--" else duration}min",
     type = type
 )
