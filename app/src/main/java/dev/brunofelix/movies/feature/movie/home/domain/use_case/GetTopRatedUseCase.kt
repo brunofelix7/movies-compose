fe3b865cf8ae8d6ada255a2ec176526f0252ed48
bruno <brunofelix.dev@gmail.com>
2026-08-19 19:34:53 -0300

    ♻️ refactor(movie): update movie use cases to return Resource instead of PagingData flow

--- app/src/main/java/dev/brunofelix/movies/feature/movie/home/domain/use_case/GetTopRatedUseCase.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/movie/home/domain/use_case/GetTopRatedUseCase.kt
@@ -1,20 +1,18 @@
 package dev.brunofelix.movies.feature.movie.home.domain.use_case
 
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import dev.brunofelix.movies.core.domain.model.Movie
 import dev.brunofelix.movies.core.domain.repository.MovieRepository
-import kotlinx.coroutines.flow.Flow
+import dev.brunofelix.movies.core.domain.util.Resource
 import javax.inject.Inject
 
 fun interface GetTopRatedUseCase {
-    operator fun invoke(): Flow<PagingData<Movie>>
+    suspend operator fun invoke(page: Int): Resource<List<Movie>>
 }
 
 class GetTopRatedUseCaseImpl @Inject constructor(
     private val repository: MovieRepository
 ) : GetTopRatedUseCase {
-    override fun invoke(): Flow<PagingData<Movie>> {
-        return repository.getTopRatedMovies(PagingConfig(pageSize = 20))
+    override suspend fun invoke(page: Int): Resource<List<Movie>> {
+        return repository.getTopRatedMovies(page)
     }
 }
--- app/src/main/java/dev/brunofelix/movies/feature/movie/home/domain/use_case/GetUpcomingUseCase.kt
+++ app/src/main/java/dev/brunofelix/movies/feature/movie/home/domain/use_case/GetUpcomingUseCase.kt
@@ -1,20 +1,18 @@
 package dev.brunofelix.movies.feature.movie.home.domain.use_case
 
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import dev.brunofelix.movies.core.domain.model.Movie
 import dev.brunofelix.movies.core.domain.repository.MovieRepository
-import kotlinx.coroutines.flow.Flow
+import dev.brunofelix.movies.core.domain.util.Resource
 import javax.inject.Inject
 
 fun interface GetUpcomingUseCase {
-    operator fun invoke(): Flow<PagingData<Movie>>
+    suspend operator fun invoke(page: Int): Resource<List<Movie>>
 }
 
 class GetUpcomingUseCaseImpl @Inject constructor(
     private val repository: MovieRepository
 ) : GetUpcomingUseCase {
-    override fun invoke(): Flow<PagingData<Movie>> {
-        return repository.getUpcomingMovies(PagingConfig(pageSize = 20))
+    override suspend fun invoke(page: Int): Resource<List<Movie>> {
+        return repository.getUpcomingMovies(page)
     }
 }
