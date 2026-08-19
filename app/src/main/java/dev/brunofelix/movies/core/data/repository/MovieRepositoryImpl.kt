b70d15a7a4e6b7bfc87c4c5a6017c6355600bc16
bruno <brunofelix.dev@gmail.com>
2026-08-19 14:48:16 -0300

    ♻️ refactor(movie): refactor MovieRepositoryImpl to use asPagerFlow extension function

--- app/src/main/java/dev/brunofelix/movies/core/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/dev/brunofelix/movies/core/data/repository/MovieRepositoryImpl.kt
@@ -1,14 +1,10 @@
 package dev.brunofelix.movies.core.data.repository
 
-import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import dev.brunofelix.movies.core.data.remote.source.MovieRemoteDataSource
-import dev.brunofelix.movies.core.domain.model.Movie
+import dev.brunofelix.movies.core.data.util.extension.asPagerFlow
 import dev.brunofelix.movies.core.domain.repository.MovieRepository
-import dev.brunofelix.movies.core.domain.util.Resource
 import dev.brunofelix.movies.core.domain.util.toResource
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 /**
@@ -20,34 +16,19 @@ class MovieRepositoryImpl @Inject constructor(
     private val remoteDataSource: MovieRemoteDataSource
 ) : MovieRepository {
 
-    override suspend fun getDetails(id: Long): Resource<Movie> {
-        return remoteDataSource.getDetails(id).toResource()
-    }
+    override suspend fun getDetails(
+        id: Long
+    ) = remoteDataSource.getDetails(id).toResource()
 
-    override fun getPopularMovies(pagingConfig: PagingConfig): Flow<PagingData<Movie>> {
-        return Pager(
-            config = pagingConfig,
-            pagingSourceFactory = {
-                remoteDataSource.getPopularPagingSource()
-            }
-        ).flow
-    }
+    override fun getPopularMovies(
+        pagingConfig: PagingConfig
+    ) = pagingConfig.asPagerFlow { remoteDataSource.getPopularPagingSource() }
 
-    override fun getUpcomingMovies(pagingConfig: PagingConfig): Flow<PagingData<Movie>> {
-        return Pager(
-            config = pagingConfig,
-            pagingSourceFactory = {
-                remoteDataSource.getUpcomingPagingSource()
-            }
-        ).flow
-    }
+    override fun getUpcomingMovies(
+        pagingConfig: PagingConfig
+    ) = pagingConfig.asPagerFlow { remoteDataSource.getUpcomingPagingSource() }
 
-    override fun getTopRatedMovies(pagingConfig: PagingConfig): Flow<PagingData<Movie>> {
-        return Pager(
-            config = pagingConfig,
-            pagingSourceFactory = {
-                remoteDataSource.getTopRatedPagingSource()
-            }
-        ).flow
-    }
+    override fun getTopRatedMovies(
+        pagingConfig: PagingConfig
+    ) = pagingConfig.asPagerFlow { remoteDataSource.getTopRatedPagingSource() }
 }
