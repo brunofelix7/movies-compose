4935198647a453af7d02150cbd31631599b71c29
Bruno Felix <brunofelix.dev@gmail.com>
2026-09-15 22:53:57 -0300

    🚚 refactor(movie): move MovieDetailViewModelTest to detail package and update tests to include GetMovieVideosUseCase

--- app/src/test/java/dev/brunofelix/movies/feature/movie/detail/presentation/ui/MovieDetailViewModelTest.kt
+++ app/src/test/java/dev/brunofelix/movies/feature/movie/detail/presentation/ui/MovieDetailViewModelTest.kt
@@ -1,18 +1,19 @@
-package dev.brunofelix.movies.feature.movie.presentation.viewmodel
+package dev.brunofelix.movies.feature.movie.detail.presentation.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.asLiveData
 import com.google.common.truth.Truth.assertThat
 import dev.brunofelix.movies.core.data.remote.mapper.toDomain
-import dev.brunofelix.movies.core.data.util.extension.toUiText
+import dev.brunofelix.movies.core.domain.model.Video
 import dev.brunofelix.movies.core.domain.use_case.DeleteMediaUseCase
 import dev.brunofelix.movies.core.domain.use_case.IsFavoriteMediaUseCase
 import dev.brunofelix.movies.core.domain.use_case.SaveMediaUseCase
 import dev.brunofelix.movies.core.domain.util.Resource
 import dev.brunofelix.movies.core.presentation.mapper.toUiModel
 import dev.brunofelix.movies.core.presentation.util.UiState
+import dev.brunofelix.movies.core.presentation.util.extension.toUiText
 import dev.brunofelix.movies.feature.movie.detail.domain.use_case.GetMovieDetailUseCase
-import dev.brunofelix.movies.feature.movie.detail.presentation.ui.MovieDetailViewModel
+import dev.brunofelix.movies.feature.movie.detail.domain.use_case.GetMovieVideosUseCase
 import dev.brunofelix.movies.test_util.MainDispatcherRule
 import dev.brunofelix.movies.test_util.factory.MovieDtoFactory
 import dev.brunofelix.movies.test_util.fake.FakeMovie
@@ -34,10 +35,11 @@ class MovieDetailViewModelTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    val getMovieDetailUseCase = mockk<GetMovieDetailUseCase>()
-    val saveMediaUseCase = mockk<SaveMediaUseCase>()
-    val isFavoriteMediaUseCase = mockk<IsFavoriteMediaUseCase>()
-    val deleteMediaUseCase = mockk<DeleteMediaUseCase>()
+    private val getMovieDetailUseCase = mockk<GetMovieDetailUseCase>()
+    private val getMovieVideosUseCase = mockk<GetMovieVideosUseCase>()
+    private val saveMediaUseCase = mockk<SaveMediaUseCase>()
+    private val isFavoriteMediaUseCase = mockk<IsFavoriteMediaUseCase>()
+    private val deleteMediaUseCase = mockk<DeleteMediaUseCase>()
 
     private lateinit var viewModel: MovieDetailViewModel
 
@@ -45,6 +47,7 @@ class MovieDetailViewModelTest {
     fun setUp() {
         viewModel = MovieDetailViewModel(
             getMovieDetailUseCase,
+            getMovieVideosUseCase,
             saveMediaUseCase,
             isFavoriteMediaUseCase,
             deleteMediaUseCase
@@ -55,10 +58,17 @@ class MovieDetailViewModelTest {
     fun `when GetMovieDetailsUseCase get success, then returns 'Success' in uiState`() = runTest {
         // Arrange
         val movie = MovieDtoFactory().create(FakeMovie.JohnWick).toDomain()
-        val movieUiState = movie.toUiModel()
+        val movieUiState = movie.toUiModel().copy(trailerKey = "abc")
         val expectedState = UiState.Success(movieUiState)
 
         coEvery { getMovieDetailUseCase(1) } returns Resource.Success(movie)
+        coEvery { getMovieVideosUseCase(1) } returns Resource.Success(listOf(
+            Video(
+                site = "YouTube",
+                type = "Trailer",
+                key = "abc"
+            )
+        ))
         coEvery { isFavoriteMediaUseCase(movie.id) } returns false
 
         // Act
@@ -75,6 +85,7 @@ class MovieDetailViewModelTest {
         val exception = Exception("Error")
         val expectedState = UiState.Error(exception.toUiText())
         coEvery { getMovieDetailUseCase(1) } returns Resource.Error(exception)
+        coEvery { getMovieVideosUseCase(1) } returns Resource.Success(emptyList())
 
         // Act
         viewModel.getDetails(1)
